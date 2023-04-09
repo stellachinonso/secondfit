991bf4d58def05605fad7b16dd6cc496ca0acd66
Stella Onyinyechi <smiles.onyinyechi@gmail.com>
2023-04-09 21:07:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const change = document.getElementById("image1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background ="red";
+    event.target.color ="white";
+});
+
+const change2 = document.getElementById("image2");
+
+change2.addEventListener("click", (event) => {
+    event.target.style.background ="green";
+    event.target.color ="white";
+});
+
+const change3 = document.getElementById("image3");
+
+change3.addEventListener("click", (event) => {
+    event.target.style.background ="red";
+    event.target.color ="blue";
+});
+
+const change4 = document.getElementById("image4");
+
+change4.addEventListener("click", (event) => {
+    event.target.style.background ="orange";
+    event.target.color ="yellow";
+});
+
+const change5 = document.getElementById("image5");
+
+change5.addEventListener("click", (event) => {
+    event.target.style.background ="pink";
+    event.target.color ="brown";
+});
+
+const change6 = document.getElementById("image6");
+
+change6.addEventListener("click", (event) => {
+    event.target.style.background ="silver";
+    event.target.color ="green";
+});
+
+const change7 = document.getElementById("image7");
+
+change7.addEventListener("click", (event) => {
+    event.target.style.background ="gold";
+    event.target.color ="indigo";
+});
+
+const change8 = document.getElementById("image8");
+
+change8.addEventListener("click", (event) => {
+    event.target.style.background ="purple";
+    event.target.color ="white";
+});
+
+const change9 = document.getElementById("image9");
+
+change9.addEventListener("click", (event) => {
+    event.target.style.background ="teal";
+    event.target.color ="indigo";
+});
